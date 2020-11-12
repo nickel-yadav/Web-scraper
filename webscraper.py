f42deb30d52da91630c49ce2a13a191241b40f28
nickel-yadav <nickelyadav57@gmail.com>
2020-11-12 12:53:09 +0530

    json object created

--- webscraper.py
+++ webscraper.py
@@ -5,6 +5,12 @@ url = 'https://ethans_fake_twitter_site.surge.sh/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content,"html.parser")
 
-tweet = content.find('p',attrs={"class":"content"}).text
-for tweet in content.findAll('p',attrs={"class":"content"}):
-    print (tweet.text.encode('utf-8'))
+tweet = []
+for tweet in content.findAll('div',attrs={"class":"tweetcontainer"}):
+    tweetObject = {
+    "author" : tweet.find('h2', attrs={"class":"author"}).text.encode('utf-8'),
+    "date": tweet.find('h5', attrs={"class": "dateTime"}).text.encode('utf-8'),
+    "tweet": tweet.find('p', attrs={"class": "content"}).text.encode('utf-8'),
+    "likes": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
+    "shares": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
+    }
